4d46dcdfdb00b2ded1b21a99c1a84f6993fdbf0a
ArtVotrex <feikq777@gmail.com>
2024-11-22 16:24:12 +0300

    Реализован перебор айпишников в виде функции.
    функция возвращает отсортированный словарь где сначала идут доступные порты, ключ - порт, значение - строка

--- Core/logic.py
+++ Core/logic.py
@@ -1,1 +1,27 @@
-i
+import socket
+from datetime import datetime
+import sys
+from data_ports import ports
+def scan_all_ports(api_to_scan : str) -> dict:
+    all_ports = dict()
+    ip = socket.gethostbyname(api_to_scan)
+    for port in ports:
+        cont = socket.socket()
+        cont.settimeout(1)
+        try:
+            cont.connect((ip, port))
+        except socket.error:
+            all_ports[port] = "Закрыт"
+        else:
+            all_ports[port] = "Открыт"
+            # print(f"{socket.gethostbyname(ip)}:{str(port)} is open/{ports[port]}")
+            cont.close()
+    all_ports_sorted = {x[0]: x[1] for x in sorted(all_ports.items(), key = lambda item: item[1] == "Закрыт")}
+    return all_ports_sorted
+    # input("Press Enter to the exit....")
+
+print(scan_all_ports("192.168.161.191"))
+
+
+
+
