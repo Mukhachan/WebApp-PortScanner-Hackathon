62081718670d6579342d89224cf57da7345101fe
ArtVotrex <feikq777@gmail.com>
2024-11-22 20:25:08 +0300

    написаны функции проверки и чтение данных

--- Core/console_app.py
+++ Core/console_app.py
@@ -1,5 +1,27 @@
+import sys
 from logic import scan_all_ports
+from format_vxod import format_cidr, format_range
 from datetime import datetime
+from correct_input import correct_ipe, correct_cidr
+import re
+
+vxod = [x for x in re.split(r"[;,\s]", input()) if len(x) != 0]
+format_vxod = []
+
+for i in range(len(vxod) - 1):
+    if "/" in vxod[i]:
+        correct_cidr(vxod[i])
+        format_vxod.extend(format_cidr(vxod[i]))
+    if "-" in vxod[i]:
+        try:
+            correct_ipe(vxod[i - 1])
+            correct_ipe(vxod[i + 1])
+            format_vxod.extend(format_range(vxod[i - 1], vxod[i + 1]))
+        except IndexError:
+            raise IndexError("Некорректные входные данные")
+    correct_ipe(vxod[i])
+    format_vxod.append(vxod[i])
+    
 
 
 
---
+++ Core/correct_input.py
@@ -0,0 +1,30 @@
+def correct_ipe(ip: str) -> bool:
+    oshib = ValueError("Некорректный айпиадрес")
+    if ip.find('.') == -1 or ip.count('.') != 3:
+        raise oshib
+    correct = ip.split('.')
+    try:
+        for x in correct:
+            x = int(x)
+            if not (0 <= x <= 254):
+                raise ValueError
+    except ValueError:
+        raise ValueError("В айпиадресе содержатся некорретные символы")
+    return True
+
+def correct_cidr(ip: str) -> bool:
+    oshib = ValueError("Некорректная CIDR-нотация")
+    if ip.find('/') == -1:
+        raise oshib
+    correct = ip.split('/')
+    if len(correct) != 2:
+        raise oshib
+    correct_ipe(correct[0])
+    try:
+        x = int(correct[1])
+        if not isinstance(x, int) and not (0 <= x <= 32):
+            raise ValueError
+    except ValueError:
+        raise ValueError("В записи CIDR-нотации содержатся некорректные символы")
+    return True
+
