6f2f32b7ad3e8b0bc093240090715964f435b3e4
ArtVortex98767 <feikq777@gmail.com>
2024-11-22 17:33:04 +0300

    Update logic.py
    убраны не нужные коменты

--- Core/logic.py
+++ Core/logic.py
@@ -2,6 +2,7 @@ import socket
 from datetime import datetime
 import sys
 from data_ports import ports
+
 def scan_all_ports(api_to_scan : str) -> dict:
     all_ports = dict()
     ip = socket.gethostbyname(api_to_scan)
@@ -14,14 +15,6 @@ def scan_all_ports(api_to_scan : str) -> dict:
             all_ports[port] = "Закрыт"
         else:
             all_ports[port] = "Открыт"
-            # print(f"{socket.gethostbyname(ip)}:{str(port)} is open/{ports[port]}")
             cont.close()
     all_ports_sorted = {x[0]: x[1] for x in sorted(all_ports.items(), key = lambda item: item[1] == "Закрыт")}
     return all_ports_sorted
-    # input("Press Enter to the exit....")
-
-print(scan_all_ports("192.168.161.191"))
-
-
-
-
