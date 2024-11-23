8cad24d5c23f5d0cf761598ecf1ecf1cd765a818
RebornedTigrex <157234124+RebornedTigrex@users.noreply.github.com>
2024-11-23 13:14:07 +0300

    Небольшой рефакторинг.
    Переименованы переменные, файлы

--- Core/console_app.py
+++ Core/console_app.py
@@ -1,11 +1,10 @@
 from logic import scan_all_ports
 from datetime import datetime
 import PySimpleGUI as sg
-from classes_for_vvod import Read_vxod
+from Core.input_correction import Read_input_
     
 "192.168.1.1 - 192.168.3.3; 192.168.1.1, 192.168.32.1; 192.168.1.1212"
 
-vxod_data = Read_vxod()
 
 
 
