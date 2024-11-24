f02bfb745494bbd875e8c5c51909e9895549d692
Dronnel <dategreng@gmail.com>
2024-11-24 12:49:25 +0300

    Изменён запрос к API

--- Api/main_api.py
+++ Api/main_api.py
@@ -3,6 +3,7 @@ import time
 from typing import List, Annotated
 from fastapi import Query
 from fastapi import FastAPI, Request, Depends, Body
+from fastapi import FastAPI, UploadFile, File
 from typing import Union
 from fastapi.responses import FileResponse, PlainTextResponse, JSONResponse, StreamingResponse, Response
 from fastapi.middleware.cors import CORSMiddleware
@@ -32,7 +33,14 @@ app.add_middleware(
 
 class CheckIPParams(BaseModel):
     info_type: str
+    body: bytes
+
+class Banner(BaseModel):
+    file: bytes
+
+
     body: dict
+
 
 #@app.get("/", response_class=JSONResponse)
 #def root():
@@ -41,9 +49,22 @@ class CheckIPParams(BaseModel):
 #    return JSONResponse(content=data)
 
 
-@app.get("/")
-def root(request: Request):    
-    print(request.query_params)
+
+
+@app.get("/ip/{ipt}")
+async def ip_get(ipt: str):
+    print(ipt)
+
+
+@app.post("/file/")
+async def file_post(file: bytes = File()):
+    print(1)
+    print(file)
+
+
+
+
+
 
 @app.get("/check_ip/{ips_text}/", response_class=PlainTextResponse)
 async def check_ip(ips_text: str):
@@ -56,7 +77,7 @@ async def check_ip(ips_text: str):
     headers = {'Content-Disposition': 'inline; name="out_pdf"; filename="out.pdf"'}
     return Response(pdf_db, headers=headers, media_type='application/pdf')
 
-@app.get("/check_ip/{ips_text}/csv")#, response_class=PlainTextResponse)
+@app.get("/check_ip/{ips_text}/csv")
 async def check_ip(ips_text: str):
     data = json.load(open('example.json', 'r'))
     csv_db = tofile.new_csv(data)
